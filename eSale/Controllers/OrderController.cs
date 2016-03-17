f5d91344279bca6fd99eaaf850d8570e8cdef41b
xiaozi0825 <tetty60213@gmail.com>
2016-03-17 14:38:24 +0800

    first commit

---
+++ eSale/Controllers/OrderController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eSale.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: Order
+        public ActionResult Index()
+        {
+            Models.OrderService orderService = new Models.OrderService();
+            var order = orderService.GetOrderById("111");
+            ViewBag.CustomerID = order.CustomerID;
+            return View();
+        }
+    }
+}
---
+++ eSale/Models/OrderService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace eSale.Models
+{
+    public class OrderService
+    {
+        /// <summary>
+        /// 新增
+        /// </summary>
+        public void InsertOrder()
+        {
+
+        }
+
+        /// <summary>
+        /// 刪除
+        /// </summary>
+        public void DeleteOrderById()
+        {
+
+        }
+
+        /// <summary>
+        /// 更新
+        /// </summary>
+        public void UpdateOrder()
+        {
+
+        }
+
+        /// <summary>
+        /// 取得
+        /// </summary>
+        /// <param name="id">訂單ID</param>
+        /// <returns></returns>
+        public Models.Order GetOrderById(string id)
+        {
+            Models.Order result = new Order();
+            result.CustomerID = "GSS";
+
+            return result;
+        }
+
+        public List<Models.Order> GetOrders()
+        {
+            return new List<Order>();
+        }
+
+    }
+}
